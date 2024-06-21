62e742151586bc321c8c17f653cce4f59e523add
Yash Thakur <117333139+yashthakur16@users.noreply.github.com>
2024-06-21 18:25:12 +0530

    Create Longest Increasing Subsequence.cpp
    Given an array a[ ] of n integers, find the Length of the Longest Strictly Increasing Subsequence.
    
    A sequence of numbers is called "strictly increasing" when each term in the sequence is smaller than the term that comes after it.
    
    Example 1:
    
    Input: n = 6, a[ ] = {5,8,3,7,9,1}
    Output: 3
    Explanation: There are more than one LIS in this array.  
    One such Longest increasing subsequence is {5,7,9}.
    Example 2:
    
    Input: n = 16, a[ ] = {0,8,4,12,2,10,6,14,1,9,5,13,3,11,7,15}
    Output: 6
    Explanation: There are more than one LIS in this array. 
    One such Longest increasing subsequence is {0,2,6,9,13,15}.
    Your Task:
    You do not need to read input or print anything. Complete the function longestSubsequence() which takes the given array and its size as input parameters and returns the length of the longest increasing subsequence.
    
    Expected Time Complexity : O( n*log(n) )
    Expected Auxiliary Space: O(n)
    
    Constraints:
    1 ≤ n ≤ 104
    0 ≤ a[i] ≤ 106

---
+++ Longest Increasing Subsequence.cpp
@@ -0,0 +1,159 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    
+    int solveS(int a[],int n)
+    {
+        vector<int> curr(n+1,0);
+        vector<int> next(n+1,0);
+        
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int prev=i-1;prev>=-1;prev--)
+            {
+                int include=0;
+                
+                if(prev==-1||a[i]>a[prev])
+                {
+                    include=1+next[i+1];
+                }
+                
+                int exclude=next[prev+1];
+                
+                curr[prev+1]=max(include,exclude);        
+            }
+            next=curr;
+        }
+        
+        
+        
+        return next[0];
+        
+        
+        
+    }
+    
+    int solveT(int a[],int n)
+    {
+        
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        
+        
+        for(int i=n-1;i>=0;i--)
+        {
+            for(int prev=i-1;prev>=-1;prev--)
+            {
+                int include=0;
+                
+                if(prev==-1||a[i]>a[prev])
+                {
+                    include=1+dp[i+1][i+1];
+                }
+                
+                int exclude=dp[i+1][prev+1];
+                
+                dp[i][prev+1]=max(include,exclude);        
+            }
+        }
+        
+        
+        
+        return dp[0][0];
+        
+        
+        
+    }
+    
+    int solveM(int a[],int n, int i,int prev,vector<vector<int>> &dp)
+    {
+        if(i>=n)
+        {
+            return 0;
+        }
+        
+        if(dp[i][prev+1]!=-1)
+        {
+            return dp[i][prev+1];
+        }
+        
+        int include=0;
+        
+        if(prev==-1||a[i]>a[prev])
+        {
+            include=1+solveM(a,n,i+1,i,dp);
+        }
+        
+        int exclude=solveM(a,n,i+1,prev,dp);
+        
+        dp[i][prev+1]=max(include,exclude);
+        
+        return dp[i][prev+1];
+        
+        
+        
+    }
+    
+    int solve(int a[],int n, int i,int prev)
+    {
+        if(i>=n)
+        {
+            return 0;
+        }
+        
+        int include=0;
+        
+        if(a[i]>prev)
+        {
+            include=1+solve(a,n,i+1,a[i]);
+        }
+        
+        int exclude=solve(a,n,i+1,prev);
+        
+        return max(include,exclude);
+        
+        
+        
+    }
+    int longestSubsequence(int n, int a[])
+    {
+        // return solve(a,n,0,-1);
+        // vector<vector<int>> dp(n+1,(vector<int>(n+1,-1)));
+        // return solveM(a,n,0,-1,dp);
+        
+        // return solveT(a,n);
+        
+        return solveS(a,n);
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    //taking total testcases
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        //taking size of array
+        cin>>n;
+        int a[n];
+        
+        //inserting elements to the array
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        //calling method longestSubsequence()
+        cout << ob.longestSubsequence(n, a) << endl;
+    }
+}
+
+// } Driver Code Ends
