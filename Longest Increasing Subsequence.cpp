5e0a08a5691fe784a7c2bbc8d37e27f80f9dcf45
Yash Thakur <117333139+yashthakur16@users.noreply.github.com>
2024-06-21 18:44:19 +0530

    Update Longest Increasing Subsequence.cpp

--- Longest Increasing Subsequence.cpp
+++ Longest Increasing Subsequence.cpp
@@ -9,6 +9,35 @@ class Solution
 {
     public:
     //Function to find length of longest increasing subsequence.
+
+int solveOptimal(int a[],int n)
+    {
+        if(n==0)
+        {
+            return 0;
+        }
+        vector<int> ans;
+        ans.push_back(a[0]);
+        
+        for(int i=1;i<n;i++)
+        {
+            if(a[i]>ans.back())
+            {
+                ans.push_back(a[i]);
+            }
+            else
+            {
+                //find the index of just next big element
+                int index=lower_bound(ans.begin(),ans.end(),a[i])-ans.begin();
+                
+                ans[index]=a[i];
+            }
+        }
+        
+        return ans.size();
+        
+        
+    }
     
     int solveS(int a[],int n)
     {
@@ -131,7 +160,8 @@ class Solution
         
         // return solveT(a,n);
         
-        return solveS(a,n);
+       // return solveS(a,n);
+        solveOptimal(a,n);
     }
 };
 
